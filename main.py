0c842bb12e426d3327e0cb0ad4d1bf017aae8135
giuseppe <giuseppeabn@gmail.com>
2020-12-26 14:16:49 -0300

    publictweet v2.0

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tweepy
 import datetime
 
-credentials = open("credentials.txt",'r').read().split('\n')
+credentials = open("credentials.txt", 'r').read().split('\n')
 
 consumer_key = credentials[0]
 consumer_secret = credentials[1]
@@ -9,26 +9,16 @@ access_token = credentials[2]
 access_token_secret = credentials[3]
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+
 def publictweet():
-    if datetime.date.today().weekday() == 0:
-        tweettopublish = 'Hi everyone, today is Monday.   #Monday '
-    if datetime.date.today().weekday() == 1:
-        tweettopublish = 'Enjoy your Tuesday.  #Tuesday'
-    if datetime.date.today().weekday() == 2:
-        tweettopublish = 'Third week of the Week. #Wednesday'
-    if datetime.date.today().weekday() == 3:
-        tweettopublish = 'Thursday. I cannot wait for the Weekend'
-    if datetime.date.today().weekday() == 4:
-        tweettopublish = 'Friday...Finally'
-    if datetime.date.today().weekday() == 5:
-        tweettopublish = 'Great it is Saturday #weekend #Saturday'
-    if datetime.date.today().weekday() == 6:
-        tweettopublish = 'Sunday morning...#Weekend #enjoy'
-    api.update_status(tweettopublish)
-    print(tweettopublish)
+    print('Escriba el tweet que será publicado')
+    print('*Cuando termine solo presione enter')
+    string = str(input())
+    api.update_status(string)
+    print('Mensaje publicado con exito',string)
 
 publictweet()
